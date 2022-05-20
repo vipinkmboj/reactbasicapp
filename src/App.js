63bab252ef90956f3932b7f75821f01300c41f51
Vipin Kumar <vipinkmboj21@gmail.com>
2022-05-20 10:51:58 +0530

    change

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with Vipin
+          Learn React with Vipin Kumar
         </a>
       </header>
     </div>
